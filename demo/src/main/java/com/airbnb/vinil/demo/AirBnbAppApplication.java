ec89d7b87846244eca8a3f632b358d67652e6b06
Vinil <vinilsmonteiro@gmail.com>
2026-01-19 01:15:48 +0530

    Initial commit - Spring Boot Airbnb clone

---
+++ demo/src/main/java/com/airbnb/vinil/demo/AirBnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.airbnb.vinil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/airbnb/vinil/demo/AirBnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb.vinil.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
